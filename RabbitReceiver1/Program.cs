ff24aa2807f8a19d480fee1f53a4f30290334c59
PedroMarquesFr <pedromarques1011@gmail.com>
2023-08-31 17:42:49 -0300

    feat: receiver

--- RabbitReceiver1/Program.cs
+++ RabbitReceiver1/Program.cs
@@ -17,7 +17,8 @@ string queueName = "DemoQueue";
 channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
 channel.QueueDeclare(queueName, false, false, false, null);
 channel.QueueBind(queueName, exchangeName, routingKey, null);
+
 //prefetchSize => tamanho da mensagem, 0 eh q a gnt nao liga pro tamanho,
 //prefetchCount => quantas mensagens vao ser enviadas de uma vez, 1 significa q vai ser processada uma mensagem por vez
-//bool global
+//bool global => aplica para todas as instancias ou so pra essa, flase é q é só pra essa
 channel.BasicQos(0, 1, false);
